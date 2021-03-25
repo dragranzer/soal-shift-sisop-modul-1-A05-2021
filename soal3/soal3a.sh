5bd849d0c4fd846cd85c1eed2974b225c410c5bb
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-03-25 09:30:34 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 for ((num=0; num<23; num=num+1))
 do
-    wget https://loremflickr.com/320/240/kitten
-    exec &> Foto$num.log
+  wget https://loremflickr.com/320/240/kitten
+  exec &>> Foto.log
 done
+
+fdupes -r ~/Sisop_Prak1/Soal_3 > my_duplicate_files.txt
+fdupes -rdN ~/Sisop_Prak1/Soal_3 > my_duplicate_files.txt
