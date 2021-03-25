01ea3d7d46ba86fb867adbeec5f0a68cbd8c80b6
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-03-25 23:13:49 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-
-for ((num=0; num<23; num=num+1))
-do
-  wget https://loremflickr.com/320/240/kitten
+string=""
+num=1
+no=1
+declare -A array
+while [[ $num -lt 24 ]]; do
+  if [ $no -lt 10 ]
+    then
+      string="0$no"
+    else
+      string=$no
+  fi
+  wget -cO Koleksi_$string.jpg - https://loremflickr.com/320/240/kitten
   exec &>> Foto.log
-done
+  md5str=($(md5sum Koleksi_$string.jpg))
 
-fdupes -r ~/Sisop_Prak1/Soal_3 > my_duplicate_files.txt
-fdupes -rdN ~/Sisop_Prak1/Soal_3 > my_duplicate_files.txt
-rm my_duplicate_files.txt
+  if [[ ${array[$md5str]} == 1 ]]
+  then
+    rm Koleksi_$string.jpg
+  else
+    array[$md5str]=1
+    no=$((no+1))
+  fi
+  num=$((num+1))
+done
