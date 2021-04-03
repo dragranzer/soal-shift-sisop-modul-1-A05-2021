b74aacc80b8942f9940f93e0e7e9cb7679b67c8b
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-04-03 21:26:57 +0700

    Update soal3c.sh

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -60,33 +60,3 @@ else
     
   done
 fi
-else
-  temp="Kucing_$today"
-  mkdir $temp
-  string=""
-  num=1
-  no=1
-  declare -A array
-  while [[ $num -lt 24 ]]; do
-    if [ $no -lt 10 ]
-      then
-        string="0$no"
-      else
-        string=$no
-    fi
-    wget -cO Kucing_$string.jpg - https://loremflickr.com/320/240/kitten
-    exec &>> Foto.log
-    md5str=($(md5sum Kucing_$string.jpg))
-
-    if [[ ${array[$md5str]} == 1 ]]
-    then
-      rm Kucing_$string.jpg
-    else
-      array[$md5str]=1
-      mv Kucing_$string.jpg $temp/Kucing_$string.jpg
-      no=$((no+1))
-    fi
-    num=$((num+1))
-    
-  done
-fi
